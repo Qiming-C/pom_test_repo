c14e37ba9221021e90d3da31dc63796585cc71cd
Jimmy Chen <49624964+Qiming-C@users.noreply.github.com>
2022-09-26 17:05:19 -0400

    change branch name

--- .lift/make-pr.sh
+++ .lift/make-pr.sh
@@ -18,7 +18,7 @@ fi
 if [[ "$3" == "run" ]] ; then
     echo "This is some new contents for some file.\n" >> the_new_file
     # branch=$(git rev-parse --abbrev-ref HEAD)
-    branch=LIFT-4445
+    branch=Lift-pr
     git add the_new_file 1>&2
     git commit -m 'New commit message' 1>&2
     echo "{\"warnings\" : [], \"tool_notes\" : [], \"pull_request\" : { \"title\" : \"PR from a V5 tool\", \"body\" : \"foo bar baz\", \"source_commit\" : \"$(git rev-parse HEAD)\", \"target_branch\" : \"${branch}\" }}"
